7fed78bb353bd653babaef314cc48c798ec93fb9
xhao <2510383889@qq.com>
2023-08-11 11:15:30 +0800

    update

--- pre_learning/chapter7/GoogleNet0811.py
+++ pre_learning/chapter7/GoogleNet0811.py
@@ -2,40 +2,34 @@ import sys,os
 sys.path.append(os.getcwd())
 from pathlib import Path
 from torch import nn
+from torch.nn import functional as F
 import torch
 
-def vgg_block(num_convs, in_channels, out_channels):
-    layers = []
-    for _ in range(num_convs):
-        layers.append(nn.Conv2d(
-            in_channels, out_channels, kernel_size=3, padding=1
-        ))
-        layers.append(nn.ReLU())
-        in_channels = out_channels
-    layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
-    return nn.Sequential(*layers)
+class Inception(nn.Moudle):
+    def __init__(self, inchannels, c1,c2,c3,c4,**kwargs):
+        super(Inception, self).__init__(**kwargs)
+        # 线路1，单1x1卷积层
+        self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+        # 线路2，1x1卷积层后接3x3卷积层
+        self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+        # 线路3，1x1卷积层后接5x5卷积层
+        self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        # 线路4，3x3最⼤汇聚层后接1x1卷积层
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
 
-def vgg(conv_arch):
-    conv_blks = []
-    in_channels = 1
-    for (num_convs, out_channels) in conv_arch:
-        conv_blks.append(vgg_block(num_convs, in_channels, out_channels))
-        in_channels = out_channels
-    
-    return nn.Sequential(
-        *conv_blks, nn.Flatten(),
-        nn.Linear(out_channels * 7 * 7, 4096), nn.ReLU(),
-        nn.Dropout(0.5), nn.Linear(4096, 4096), nn.ReLU(),
-        nn.Dropout(0.5), nn.Linear(4096, 10)
-    )
+    def forward(self, x):
+        p1 = F.relu(self.p1_1(x))
+        p2 = F.relu(self.p2_2(F.relu(self.p2_1)))
+        p3 = F.relu(self.p2_3(F.relu(self.p3_1)))
+        p4 = F.relu(self.p4_2(self.p4_1(x)))
+        return torch.cat((p1, p2, p3, p4), dim=1)
+
 
 def test_shape():
-    X = torch.randn(1,1,224,224)
-    conv_arch = [(1, 64), (1, 128), (2, 256), (2, 512), (2, 512)]
-    net  = vgg(conv_arch)
-    for block in net:
-        X= block(X)
-        print(block.__class__.__name__, "out shape:\t", X.shape)
+    pass
 
 if __name__ == '__main__':
     test_shape()
